039387f09af2524bd4a376b5dc21399c5315476e
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2018-12-19 11:50:57 +0100

    refactor(localstore): remove unused

--- src/lib/core/localstorage/utils/json.ts
+++
@@ -1,8 +0,0 @@
-// TODO: do we want to log something if parsing failed or just ignore it silently?
-export const parseJSON = (key: string, json: string): any => {
-  try {
-    return JSON.parse(json);
-  } catch (e) {
-    return String(json);
-  }
-};
