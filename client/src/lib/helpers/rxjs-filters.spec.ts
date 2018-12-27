37587f6997c3951d76060b4299ca4056abf9eb1e
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2018-12-27 13:50:42 +0100

    feat(helpers): remove unused

--- client/src/lib/helpers/rxjs-filters.spec.ts
+++
@@ -1,8 +0,0 @@
-import * as filters from './rxjs-filters';
-import { of } from 'rxjs';
-
-describe('RxJs Filters', () => {
-    it('Should check if isNotNil', () => {
-        const isNil = filters.isNotNil(of(null));
-    });
-});
--- client/src/lib/helpers/rxjs-filters.ts
+++
@@ -1,4 +0,0 @@
-import { filter } from 'rxjs/operators';
-import { negate, isNil } from 'lodash-es';
-
-export const isNotNil = filter(negate(isNil));
