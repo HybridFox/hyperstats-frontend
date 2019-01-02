af92734d85e41289b31760351dfaa2d91970505d
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2019-01-02 11:16:10 +0100

    feat: update deployement

--- client/src/environments/environment.dev.ts
+++ client/src/environments/environment.dev.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:4350/server/'
+  production: true,
+  apiBaseUrl: 'https://rare-o.studiohyperdrive.be/server'
 };
 
 /*
