83a9cf29a23e7518b8d7095bc597d259e272598a
Kim Janssens <kim.janssens@district01.be>
2019-01-07 15:05:45 +0100

    feat: user menu component

---
+++ client/src/lib/ui/user-menu/user-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user-menu',
+    templateUrl: './user-menu.component.html',
+})
+
+export class UserMenuComponent {
+    visible = false;
+
+    toggle() {
+        this.visible = !this.visible;
+    }
+}
