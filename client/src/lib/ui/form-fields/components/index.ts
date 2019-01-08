36865836c1cfca7988b20f2d2f69ff59e83fe898
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2019-01-08 15:09:26 +0100

    feat(checkbox): create component

--- client/src/lib/ui/form-fields/components/index.ts
+++ client/src/lib/ui/form-fields/components/index.ts
@@ -1,7 +1,9 @@
 import { TextInputComponent } from './text-input/text-input.component';
 import { SelectInputComponent } from './select/select.component';
+import { CheckboxInputComponent } from './checkbox/checkbox.component';
 
 export const Components = [
     TextInputComponent,
-    SelectInputComponent
+    SelectInputComponent,
+    CheckboxInputComponent
 ];
