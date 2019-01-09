9a176f01fff0d14498248cb22f87264c2de7b653
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2019-01-09 13:11:21 +0100

    feat(scss): add utils

---
+++ client/src/app/core/pages/contact/contact.page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { select } from '@angular-redux/store';
+import { Subject } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+
+import { AuthActions, AuthSelector } from '@store/auth';
+
+@Component({
+    templateUrl: './contact.page.html',
+})
+export class ContactPageComponent implements OnInit, OnDestroy {
+    @select(AuthSelector.register.loading) public loading$: boolean;
+
+    public contactForm: FormGroup;
+    public componentDestroyed$: Subject<Boolean> = new Subject<boolean>();
+
+    constructor(
+        private authAction: AuthActions,
+        private toastrService: ToastrService,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+        this.contactForm = new FormGroup({
+            email: new FormControl('', Validators.required),
+            name: new FormControl('', Validators.required),
+            message: new FormControl('', Validators.required),
+        });
+    }
+
+    public ngOnDestroy() {
+        this.componentDestroyed$.next(true);
+        this.componentDestroyed$.complete();
+    }
+
+    public submit() {
+        this.authAction.register({
+            ...this.contactForm.value
+        }).then(() => {
+            // TODO: translate
+            this.toastrService.success('TOAST.REGISTER.SUCCESS.DESCRIPTION', 'TOAST.REGISTER.SUCCESS.TITLE');
+            this.contactForm.reset();
+        }).catch(() => {
+            this.toastrService.success('TOAST.REGISTER.ERROR.DESCRIPTION', 'TOAST.REGISTER.ERROR.TITLE');
+        });
+    }
+}
--- client/src/app/core/pages/index.ts
+++ client/src/app/core/pages/index.ts
@@ -1,10 +1,13 @@
+export * from './contact';
 export * from './login';
 export * from './register';
 
+import { ContactPageComponent } from './contact';
 import { LoginPageComponent } from './login';
 import { RegisterPageComponent } from './register';
 
 export const Pages = [
     LoginPageComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    ContactPageComponent
 ];
