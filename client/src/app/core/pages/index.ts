e3559ebee82a022bd185bf49e2830bcb035fd959
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2019-01-10 11:32:51 +0100

    feat: add about page

--- client/src/app/core/pages/index.ts
+++ client/src/app/core/pages/index.ts
@@ -1,10 +1,13 @@
+export * from './about';
 export * from './login';
 export * from './register';
 
+import { AboutPageComponent } from './about';
 import { LoginPageComponent } from './login';
 import { RegisterPageComponent } from './register';
 
 export const Pages = [
+    AboutPageComponent,
     LoginPageComponent,
     RegisterPageComponent
 ];
