30d5649cddd5d4439aa99c4f2270021762d13760
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2019-01-16 09:01:24 +0100

    feat(components): move under one module

---
+++ client/src/lib/shared/components/index.ts
@@ -0,0 +1,17 @@
+import { PageTitleComponent } from './page-title/page-title.component';
+import { SectionTitleComponent } from './section-title/section-title.component';
+import { SubSectionTitleComponent } from './sub-section-title/sub-section-title.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { MenuComponent } from './menu/menu.component';
+import { UserMenuComponent } from './user-menu/user-menu.component';
+
+export const Components = [
+    PageTitleComponent,
+    SectionTitleComponent,
+    SubSectionTitleComponent,
+    FooterComponent,
+    MainMenuComponent,
+    MenuComponent,
+    UserMenuComponent
+];
