1a8f2f9b8da855d5bb7b367d5a350625fa080eff
Jeroen Valcke <valcke_jeroen@hotmail.com>
2019-01-23 17:05:12 +0100

    remove company from fixtures before testing

--- server/test/mocks/startServer.js
+++ server/test/mocks/startServer.js
@@ -2,14 +2,16 @@ const createTestUser = require("../helpers/createTestUser");
 const nodemailerMock = require("nodemailer-mock");
 const mockery = require("mockery");
 const removeTestUsers = require("../helpers/removeTestUsers");
+const testCompanyhelper = require("../helpers/testCompany");
 
 module.exports = async() => {
 	mockery.enable({ warnOnUnregistered: false });
 	mockery.registerMock("nodemailer", nodemailerMock);
 
-	const server = require("../../index");
-
+	await testCompanyhelper.remove("5c485d0029abc50032947f91");
 	await createTestUser();
+
+	const server = require("../../index");
 
 	const reset = () => {
 		nodemailerMock.mock.reset();
