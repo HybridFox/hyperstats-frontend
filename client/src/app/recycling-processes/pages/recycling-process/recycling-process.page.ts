7401b4f5f4637df32f773810e6158070ebb90353
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2019-01-23 11:54:08 +0100

    feat(recycling-process): add steps

--- client/src/app/recycling-processes/pages/recycling-process/recycling-process.page.ts
+++ client/src/app/recycling-processes/pages/recycling-process/recycling-process.page.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit, } from '@angular/core';
-import { RecyclingProcessesActions } from '../../store';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recycling-process-page',
   templateUrl: './recycling-process.page.html',
 })
 export class RecyclingProcessPageComponent implements OnInit {
+    public recyclingProcessForm: any;
+
     constructor(
-        private router: Router,
-        private recyclingProcessesActions: RecyclingProcessesActions,
+        private formBuilder: FormBuilder,
     ) {}
 
     public ngOnInit() {
+        this.recyclingProcessForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            steps: this.formBuilder.array([this.createStep()])
+        });
+    }
 
+    public addStep(): void {
+        this.recyclingProcessForm.controls.steps.push(this.createStep());
+    }
+
+    private createStep(): FormGroup {
+        return this.formBuilder.group({
+            precedingStep: ['', Validators.required],
+            description: ['', Validators.required],
+            recyclingSiteOwner: ['', Validators.required],
+            methodOfProcessing: ['', Validators.required],
+            qualitativeDescription: ['', Validators.required],
+            schematicOverview: ['', Validators.required],
+        });
     }
 }
