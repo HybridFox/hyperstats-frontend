7d59a196692560f9c05e077727433ed3ef3b3a56
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2019-01-24 08:56:49 +0100

    fix(store): export correct reducer

--- client/src/app/recycling-processes/store/reducers.ts
+++ client/src/app/recycling-processes/store/reducers.ts
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import { listReducer } from './list/reducers';
 import { detailReducer } from './detail/reducers';
 
-export const customsOfficesReducers = combineReducers({
+export const recyclingProcessesReducer = combineReducers({
   list: listReducer,
   detail: detailReducer,
 });
