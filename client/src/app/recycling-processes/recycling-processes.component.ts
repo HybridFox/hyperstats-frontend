1ac072987b1f832980bca6a6fe924bdf1e4ca626
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2019-01-24 08:21:18 +0100

    fix(recycling-processes): remove viewencapsulation

--- client/src/app/recycling-processes/recycling-processes.component.ts
+++ client/src/app/recycling-processes/recycling-processes.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-recycling-processes',
-  encapsulation: ViewEncapsulation.None,
   templateUrl: './recycling-processes.component.html',
 })
 export class RecyclingProcessesComponent {}
