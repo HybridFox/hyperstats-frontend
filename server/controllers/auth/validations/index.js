3fa6af93d6369a05ec6e49f4b21399a8d367dec7
Jeroen Valcke <valcke_jeroen@hotmail.com>
2019-01-24 18:44:15 +0100

    fix: tests

--- server/controllers/auth/validations/index.js
+++ server/controllers/auth/validations/index.js
@@ -1,7 +1,7 @@
 const login = require("./login");
 const register = require("./register");
 const requestPasswordReset = require("./requestPasswordReset");
-const resetPassword = require("./requestPasswordReset");
+const resetPassword = require("./resetPassword");
 const verify = require("./verify");
 
 module.exports = {
