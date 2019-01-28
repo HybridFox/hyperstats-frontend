2e5c85c1db7bfff078dc86a46dacb1e0eea8eb46
Jasper De Smet <jasp.desmet@gmail.com>
2019-01-28 14:50:05 +0100

    feat: update swagger

--- server/routes/assets.js
+++ server/routes/assets.js
@@ -5,6 +5,37 @@ const Validations = require("../controllers/assets/validations");
 const Controller = require("../controllers/assets");
 
 module.exports = (router) => {
+	/**
+	 * @swagger
+	 * /api/assets:
+	 *   post:
+	 *     description: Create asset
+	 *     tags:
+	 *       - assets
+	 *     consumes:
+	 *       - multipart/form-data
+	 *     produces:
+	 *       - application/json
+	 *     parameters:
+	 *       - in: formData
+	 *         name: file
+	 *         required: true
+	 *         type: file
+	 *     responses:
+	 *       201:
+	 *         description: Asset created
+	 *         schema:
+	 *           type: object
+	 *           properties:
+	 *             id:
+	 *               type: string
+	 *             mimetype:
+	 *               type: string
+	 *             uploadDate:
+	 *               type: string
+	 *             originalname:
+	 *               type: string
+	 */
 	router.route("/assets")
 		.post(
 			AuthMiddleware.isLoggedIn,
@@ -13,6 +44,29 @@ module.exports = (router) => {
 			...Controller.create,
 		);
 
+	/**
+	 * @swagger
+	 * /api/assets/{id}:
+	 *   parameters:
+	 *     - in: path
+	 *       name: id
+	 *       required: true
+	 *       description: uuid
+	 *       type: string
+	 *   get:
+	 *     description: View or download asset
+	 *     tags:
+	 *       - assets
+	 *     produces:
+	 *       - any
+	 *     parameters:
+	 *       - in: query
+	 *         name: doanlow
+	 *         type: boolean
+	 *     responses:
+	 *       200:
+	 *         description: View or download asset
+	 */
 	router.route("/assets/:id")
 		.get(
 			AuthMiddleware.isLoggedIn,
