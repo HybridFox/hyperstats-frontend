b885c4aa04bd7a4cc9a33f8f50315441cc7f9aaa
Jasper De Smet <jasp.desmet@gmail.com>
2019-02-05 10:59:33 +0100

    feat: add update company endpoint

--- server/controllers/users/helpers/userManager/index.js
+++ server/controllers/users/helpers/userManager/index.js
@@ -2,5 +2,6 @@ module.exports = {
 	getAll: require("./getAll"),
 	getById: require("./getById"),
 	update: require("./update"),
+	updateCompany: require("./updateCompany"),
 	updateStatus: require("./updateStatus"),
 };
