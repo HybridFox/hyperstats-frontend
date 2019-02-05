01f46aa2a39d26cd2e83a93c99872915b5775b02
Kim Janssens <kim.janssens@district01.be>
2019-02-05 17:01:36 +0100

    feat: undo comment

--- client/src/app/recycling-partners/store/list/selectors.ts
+++ client/src/app/recycling-partners/store/list/selectors.ts
@@ -1,11 +1,10 @@
 import { selectDenormalized } from '@store/utils/selector';
 import * as schema from '@core/schemas';
 
-// export const list = selectDenormalized({
-//   schema: schema.recyclingProcess,
-//   selector: 'recyclingProcesses.list.result',
-// });
+export const list = selectDenormalized({
+  schema: schema.recyclingProcess,
+  selector: 'recyclingProcesses.list.result',
+});
 
-export const list = ['recyclingPartners', 'list', 'result'];
 export const listLoading = ['recyclingPartners', 'list', 'loading'];
 export const listError = ['recyclingPartners', 'list', 'error'];
