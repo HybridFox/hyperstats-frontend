b5ee810c19ea4b59e386373c73eb0b68a7b2eb9e
Jasper De Smet <jasp.desmet@gmail.com>
2019-02-05 09:10:09 +0100

    chore: add integration test get user by id

---
+++ server/test/integration/users/getById.spec.js
@@ -0,0 +1,67 @@
+const supertest = require("supertest");
+const { expect } = require("chai");
+const startServer = require("../../mocks/startServer");
+const createTestUser = require("../../helpers/createTestUser");
+const removeTestUsers = require("../../helpers/removeTestUsers");
+const loginUser = require("../../helpers/loginUser");
+
+describe("Integration", () => {
+	describe("Users", () => {
+		let server;
+		let closeServer;
+		let reset;
+		let cookie;
+		let userId;
+
+		before(async() => {
+			const result = await createTestUser({
+				email: "test1@example.com",
+				isAdmin: true,
+			});
+			userId = result._id;
+			await createTestUser({
+				email: "test2@example.com",
+				isAdmin: true,
+			});
+
+			const { server: s, closeServer: c, reset: r } = await startServer();
+
+			server = s;
+			closeServer = c;
+			reset = r;
+
+			cookie = (await loginUser(server)).cookie;
+		});
+
+		afterEach(() => reset());
+
+		after(async() => {
+			await removeTestUsers(["test1@example.com", "test2@example.com"]);
+			await closeServer();
+		});
+
+		describe("When not logged in", () => {
+			it("Should not fetch user by id", () => {
+				return supertest(server)
+					.get("/api/users")
+					.expect("Content-Type", /json/)
+					.expect(403);
+			});
+		});
+
+		describe("When logged in", () => {
+			it("Should fetch user by id", async() => {
+				return supertest(server)
+					.get(`/api/users/${userId}`)
+					.set("cookie", cookie)
+					.expect("Content-Type", /json/)
+					.expect(200)
+					.then(({ body }) => {
+						expect(body).to.be.an("object");
+						expect(body._id).to.equal(userId.toString());
+						expect(body.data.email).to.equal("test1@example.com");
+					});
+			});
+		});
+	});
+});
