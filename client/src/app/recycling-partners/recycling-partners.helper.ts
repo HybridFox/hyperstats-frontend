589fa6b5e6b1f79692ddc1e05dec060206b396f2
Kim Janssens <kim.janssens@district01.be>
2019-02-07 16:50:17 +0100

    feat: use select$ and separate helper file

---
+++ client/src/app/recycling-partners/recycling-partners.helper.ts
@@ -0,0 +1,15 @@
+import { map, filter } from 'rxjs/operators';
+
+export const recyclingPartners = (obs$) => {
+    return obs$
+        .pipe(
+            filter((partners: any[]) => {
+                return Array.isArray(partners);
+            }),
+            map((partners: any[]) => {
+                return partners.map((partner) => {
+                    return {'link': partner._id, 'label': partner.data.name};
+                  });
+            })
+        )
+}
