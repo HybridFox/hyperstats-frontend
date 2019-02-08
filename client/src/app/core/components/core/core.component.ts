d2b34ef9ab16cfcc77518f65f8246f8e0a345253
Jasper De Smet <jasp.desmet@gmail.com>
2019-02-08 11:42:30 +0100

    fix: login route

--- client/src/app/core/components/core/core.component.ts
+++ client/src/app/core/components/core/core.component.ts
@@ -35,7 +35,7 @@ export class CoreComponent implements OnInit {
   public logout() {
     this.authActions.logout()
       .then(() => {
-        this.router.navigate(['/login']);
+        this.router.navigate(['/', 'auth', 'login']);
       });
   }
 }
