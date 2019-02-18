905567fea62adb8c116cd70b21abab23311e54f4
DenisValcke <denis.valcke@gmail.com>
2019-02-18 11:34:58 +0100

    PR remarks: fix job validation for objects

--- server/controllers/report/validations/report.js
+++ server/controllers/report/validations/report.js
@@ -3,25 +3,25 @@ const { schemas } = require("../../../helpers/validation");
 const REPORT_STATUS = require("../helpers/const").REPORT_STATUS;
 
 const schema = joi.object().keys({
-	data: joi.object({
+	data: joi.object().keys({
 		information: {
 			reportingYear: joi.number().required(),
 			recyclingProcess: joi.string().required(),
 			name: joi.string().required(),
 			receiver: joi.string().required(),
 		},
-		inputFraction: joi.array().items(joi.object({
+		inputFraction: joi.array().items(joi.object().keys({
 			siteRef: joi.string(),
 			data: {
 				processChemistry: joi.string().required(),
 				weightInput: joi.number().required(),
 				shareOfBatteryType: joi.number().required(),
 				weightBatteryType: joi.number().required(),
-				excessMaterialReceived: joi.array().items(joi.object({
+				excessMaterialReceived: joi.array().items(joi.object().keys({
 					impurities: joi.number().required(),
 					PackagingMaterial: joi.number().required(),
 				})),
-				elements: joi.array().items(joi.object({
+				elements: joi.array().items(joi.object().keys({
 					element: joi.string().required(),
 					mass: joi.number().required(),
 				})),
@@ -31,17 +31,17 @@ const schema = joi.object().keys({
 				massOfOuterCasings: joi.number().required(),
 			},
 		})),
-		additives: joi.array().items(joi.object({
+		additives: joi.array().items(joi.object().keys({
 			type: joi.string().required(),
 			weight: joi.number().required(),
-			chemicalComposition: joi.array().items(joi.object({
+			chemicalComposition: joi.array().items(joi.object().keys({
 				element: joi.string().required(),
 				weight: joi.number().required(),
 			})),
 		})),
-		outputFraction: joi.array().items(joi.object({
+		outputFraction: joi.array().items(joi.object().keys({
 			siteRef: joi.string(),
-			data: joi.array().items(joi.object({
+			data: joi.array().items(joi.object().keys({
 				element: joi.string().required(),
 				mass: joi.number().required(),
 				classification: joi.string().required(),
@@ -54,13 +54,13 @@ const schema = joi.object().keys({
 			calculatedEfficiency: joi.number().required(),
 		},
 		additionalInformation: {
-			files: joi.array().items(joi.object({
+			files: joi.array().items(joi.object().keys({
 				type: joi.string(),
 			})),
 			additionalInformation: joi.string().required(),
 		},
 	}),
-	meta: joi.object({
+	meta: joi.object().keys({
 		approvedCompanies: joi.array().items(joi.string()).optional(),
 		status: joi.string().valid(REPORT_STATUS).optional(),
 	}),
