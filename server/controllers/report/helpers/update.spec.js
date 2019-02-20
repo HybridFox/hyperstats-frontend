8cdb0be90839426c37d7075b0edb8753d9f7d46f
DenisValcke <denis.valcke@gmail.com>
2019-02-20 17:08:14 +0100

    Expand report controller helpers tests

--- server/controllers/report/helpers/update.spec.js
+++ server/controllers/report/helpers/update.spec.js
@@ -1,11 +1,12 @@
 const { expect, use, should } = require("chai");
 const chaiAsPromised = require("chai-as-promised");
 const createObjectId = require("mongoose").Types.ObjectId;
-const { mockMongoose } = require("../../../test/mocks");
+const ResponseError = require("../../../helpers/errors/responseError");
 const createReport = require("./create");
 const updateReport = require("./update");
-const { NEW_REPORT } = require("../../../test/mocks/report");
 const { REPORT_STATUS } = require("./const");
+const { mockMongoose } = require("../../../test/mocks");
+const { NEW_REPORT } = require("../../../test/mocks/report");
 
 should();
 use(chaiAsPromised);
@@ -14,12 +15,18 @@ describe("Report", () => {
 	describe("update", () => {
 		const companyId = createObjectId();
 		let mongoServer;
-		let initialReport;
+		let savedReport;
+		let filedReport;
 
 		before(async() => {
 			mongoServer = await mockMongoose();
 
-			initialReport = await createReport({
+			savedReport = await createReport({
+				report: NEW_REPORT,
+				meta: { status: REPORT_STATUS.SAVED },
+				companyId,
+			});
+			filedReport = await createReport({
 				report: NEW_REPORT,
 				meta: { status: REPORT_STATUS.SAVED },
 				companyId,
@@ -40,7 +47,7 @@ describe("Report", () => {
 			};
 
 			const updatedReport = await updateReport({
-				_id: initialReport._id.toString(),
+				_id: savedReport._id.toString(),
 				reportedById: companyId,
 				updatedData,
 				updatedStatus: REPORT_STATUS.FILED,
@@ -49,8 +56,40 @@ describe("Report", () => {
 			expect(updatedReport).to.be.an("object");
 			expect(updatedReport.data.information.reportingYear).to.equal(2018);
 			expect(updatedReport.meta.status).to.equal(REPORT_STATUS.FILED);
-			expect(updatedReport.meta.lastUpdated).to.not.equal(initialReport.meta.lastUpdated);
-			expect(updatedReport.meta.created).to.not.equal(initialReport.meta.created);
+			expect(updatedReport.meta.lastUpdated).to.not.equal(savedReport.meta.lastUpdated);
+			expect(updatedReport.meta.created).to.not.equal(savedReport.meta.created);
+		});
+
+		it("Should not be able to update a report that's filed", async() => {
+			const updatedData = {
+				...NEW_REPORT,
+				information: {
+					...NEW_REPORT,
+					reportingYear: 2018,
+				},
+			};
+
+			expect(updateReport({
+				_id: filedReport._id.toString(),
+				reportedById: companyId,
+				updatedData,
+			})).to.eventually.rejectedWith(ResponseError);
+		});
+
+		it("Should throw an error when no report is found", () => {
+			const updatedData = {
+				...NEW_REPORT,
+				information: {
+					...NEW_REPORT,
+					reportingYear: 2018,
+				},
+			};
+
+			expect(updateReport({
+				_id: createObjectId(),
+				reportedById: companyId,
+				updatedData,
+			})).to.eventually.rejectedWith(ResponseError);
 		});
 	});
 });
