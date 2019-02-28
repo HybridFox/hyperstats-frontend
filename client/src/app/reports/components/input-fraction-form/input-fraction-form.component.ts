0d515f328e3d923f1241d02359c47bc2d1ad641b
Jochem Janssens <janssens.jochem@gmail.com>
2019-02-28 16:42:27 +0100

    chore: decrease timeout

--- client/src/app/reports/components/input-fraction-form/input-fraction-form.component.ts
+++ client/src/app/reports/components/input-fraction-form/input-fraction-form.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnChanges, Input } from '@angular/core';
-import { CodesService } from 'src/app/core/services/codes/codes.service';
-import { ToastrService } from 'ngx-toastr';
-import { Router, ActivatedRoute } from '@angular/router';
 import { FormArray } from '@angular/forms';
 
 import { FormDataService } from '../../services/formdata.service';
-import { ReportsProcessActions } from 'src/app/reports/store/recycling-processes';
-
 import { FormGroup } from '@angular/forms';
-import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-input-fraction-form',
@@ -28,7 +22,7 @@ export class InputFractionFormComponent implements OnChanges {
   public ngOnChanges() {
     setTimeout(() => {
       this.setActiveStepById();
-    }, 100);
+    }, 10);
   }
 
   private setActiveStepById() {
