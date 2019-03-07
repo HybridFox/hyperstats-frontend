7b30033591e9c3988aeb7b8a91751142879c0573
Jo Smets <josmets1997@gmail.com>
2019-03-07 12:59:45 +0100

    feat: email admin on signup request

--- server/controllers/auth/helpers/verifyHandler.js
+++ server/controllers/auth/helpers/verifyHandler.js
@@ -1,4 +1,22 @@
+const path = require("path");
 const UserModel = require("../../../models/user");
+const mailer = require("../../../helpers/mail");
+const ResponseError = require("../../../helpers/errors/responseError");
+
+const mailToAdmins = (user, admins) => mailer({
+	to: admins,
+	subject: "Rare - New Signup Request",
+	templatePath: path.resolve(process.cwd(), "controllers/auth/templates/signupRequest.html"),
+	data: {
+		userFirstName: user.data.firstname,
+		userLastName: user.data.lastname,
+		confirmPath: `/admin/users/signup-requests/${user._id}`,
+	},
+}).catch(async(error) => {
+	await user.remove();
+
+	throw new ResponseError({ type: 500, msg: "Sending mail failed", error });
+});
 
 /**
  * @function verifyHandler Get user based on verify token an make the user verified
@@ -7,12 +25,23 @@ const UserModel = require("../../../models/user");
  */
 module.exports = async(token) => {
 	const user = await UserModel.findOne({ "meta.deleted": false, "meta.validation": { isValidated: false, token } }).exec();
+	let admins = await UserModel.find({ "meta.deleted": false, "meta.isAdmin": true }).select("data.email").exec();
+
+	admins = admins.reduce((acc, admin) => {
+		acc.push(admin.data.email);
+
+		return acc;
+	}, []);
 
 	if (!user) {
 		throw new Error({ type: 400, msg: "Invalid token!" });
 	}
 
 	user.meta.validation.isValidated = true;
+
+	await Promise.all([
+		mailToAdmins(user, admins),
+	]);
 
 	await user.save();
 
