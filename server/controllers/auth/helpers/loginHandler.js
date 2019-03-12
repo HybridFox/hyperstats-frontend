c6d46b854cd3bf8bdb099ad638ce7d35da40133f
Jo Smets <josmets1997@gmail.com>
2019-03-12 15:37:46 +0100

    fix: spec test

--- server/controllers/auth/helpers/loginHandler.js
+++ server/controllers/auth/helpers/loginHandler.js
@@ -10,12 +10,12 @@ const ResponseError = require("../../../helpers/errors/responseError");
 module.exports = async(username, password) => {
 	const user = await UserModel.findOne({ "data.username": username }).exec();
 
-	if (user.meta.deleted) {
-		throw new ResponseError({ type: 401, msg: "Not authorized." });
-	}
-
 	if (!user) {
 		throw new ResponseError({ type: 404, msg: "User not found" });
+	}
+
+	if (user.meta.deleted) {
+		throw new ResponseError({ type: 401, msg: "Not authorized." });
 	}
 
 	if (!await user.validatePassword(password)) {
