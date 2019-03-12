4792537b5a14ff7dfa64ef097f84c8f3a9ad598c
Jo Smets <josmets1997@gmail.com>
2019-03-12 13:23:57 +0100

    feat: email on file report

--- server/controllers/report/helpers/update.js
+++ server/controllers/report/helpers/update.js
@@ -3,6 +3,24 @@ const ReportModel = require("../../../models/report");
 const errors = require("../../../helpers/errorHandler");
 const getReportQuery = require("./getQuery");
 const { REPORT_STATUS } = require("./const");
+const mailer = require("../../../helpers/mail");
+const ResponseError = require("../../../helpers/errors/responseError");
+const UserModel = require("../../../models/user");
+const path = require("path");
+
+
+const mailReportToUsers = (companyEmailList, id) => mailer({
+	to: companyEmailList,
+	subject: "Rare - Report filed",
+	templatePath: path.resolve(process.cwd(), "controllers/report/templates/filedReport.html"),
+	data: {
+		confirmPath: `/recycler/reports/${id}`,
+	},
+}).catch(async(error) => {
+	await companyEmailList.remove();
+
+	throw new ResponseError({ type: 500, msg: "Sending mail failed", error });
+});
 
 const getCurrentReport = async(id, query) => {
 	const report = await ReportModel.findOne(query).lean().exec();
@@ -18,7 +36,7 @@ const getCurrentReport = async(id, query) => {
 	return report;
 };
 
-const updateReport = async(id, query, updatedData, updatedStatus, updatedState, currentReport) => {
+const updateReport = async(id, query, updatedData, updatedStatus, updatedState, currentReport, reportedById) => {
 	const report = await ReportModel.findOneAndUpdate(
 		query,
 		{ $set: mergeDeepLeft({
@@ -36,12 +54,21 @@ const updateReport = async(id, query, updatedData, updatedStatus, updatedState, 
 		throw errors.ItemNotFound;
 	}
 
+	if (updatedStatus === REPORT_STATUS.FILED) {
+		const users = await UserModel.find({ "meta.deleted": false, "data.company": reportedById }).select("data.email").exec();
+		const companyEmailList = users.reduce((acc, user) => [...acc, user.data.email], []);
+
+		await Promise.all([
+			mailReportToUsers(companyEmailList, id),
+		]);
+	}
+
 	return report;
 };
 
 module.exports = async({ _id, reportedById, updatedData, updatedStatus, updatedState } = {}) => {
 	const query = getReportQuery(_id, reportedById);
-	const currentReport = await getCurrentReport(_id, query);
+	const currentReport = await getCurrentReport(_id, query, reportedById);
 
-	return await updateReport(_id, query, updatedData, updatedStatus, updatedState, currentReport);
+	return await updateReport(_id, query, updatedData, updatedStatus, updatedState, currentReport, reportedById);
 };
