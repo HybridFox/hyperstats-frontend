fd740e92e92440eb7e4d74a8840a6ceadde801be
Jochem Janssens <janssens.jochem@gmail.com>
2019-03-13 13:51:21 +0100

    feat: make correct menu for CO

--- client/src/app/core/components/app-navigation/app-navigation.component.ts
+++ client/src/app/core/components/app-navigation/app-navigation.component.ts
@@ -5,27 +5,33 @@ import { select } from '@angular-redux/store';
 import { storeRouterSelectors } from '@core/store-router';
 
 @Component({
-    selector: 'app-navigation',
-    templateUrl: './app-navigation.component.html',
+  selector: 'app-navigation',
+  templateUrl: './app-navigation.component.html',
 })
-export class AppNavigationComponent implements OnInit  {
-    @select(storeRouterSelectors.data) public routeData$: any;
-    @Input() public actionButton: ActionButton;
-    @Input() public user: any;
-    @Output() public logout: EventEmitter<any> = new EventEmitter<any>();
+export class AppNavigationComponent implements OnInit {
+  @select(storeRouterSelectors.data) public routeData$: any;
+  @Input() public actionButton: ActionButton;
+  // Why any 😢
+  @Input() public user: any;
+  @Output() public logout: EventEmitter<any> = new EventEmitter<any>();
 
-    constructor(private router: Router) { }
+  public showAddReport = false;
 
-    ngOnInit() {
-      this.router.events.subscribe((evt) => {
-          if (!(evt instanceof NavigationEnd)) {
-              return;
-          }
-          window.scrollTo(0, 0);
-      });
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+
+    // TODO: get CO and AO from an enum
+    this.showAddReport = !this.user.isAdmin && this.user.company.meta.type !== 'CO' && this.user.company.meta.type === 'AO';
   }
 
-    public onLogout() {
-        this.logout.emit();
-    }
+  public onLogout() {
+    this.logout.emit();
+  }
 }
--- client/src/lib/shared/components/main-menu/main-menu.component.ts
+++ client/src/lib/shared/components/main-menu/main-menu.component.ts
@@ -2,114 +2,125 @@ import { Component, Input, HostListener, ViewChild, ElementRef, AfterViewInit, O
 import { dropLast, takeLast, take, drop } from 'ramda';
 import { Subject } from 'rxjs';
 import { debounceTime, takeUntil } from 'rxjs/operators';
-import { _ as ngxExtract } from '@biesbjerg/ngx-translate-extract/dist/utils/utils';
+import { _ as ngxExtract } from '@biesbjerg/ngx-translate-extract/dist/utils/utils';
 
 @Component({
-    selector: 'app-main-menu',
-    templateUrl: './main-menu.component.html',
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
 })
 
 export class MainMenuComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
-    @Input() profile: any;
-    @ViewChild('container') public container: ElementRef;
-    @ViewChild('nav') public nav: ElementRef;
+  @Input() profile: any;
+  @ViewChild('container') public container: ElementRef;
+  @ViewChild('nav') public nav: ElementRef;
 
-    public navItems = [];
-    public filteredNavItems: any[] = [];
-    public extra: any[] = [];
-    public componentDestroyed$: Subject<Boolean> = new Subject<boolean>();
-    public containerWidth: Subject<number> = new Subject<number>();
-    public navWidth: Subject<number> = new Subject<number>();
+  public navItems = [];
+  public filteredNavItems: any[] = [];
+  public extra: any[] = [];
+  public componentDestroyed$: Subject<Boolean> = new Subject<boolean>();
+  public containerWidth: Subject<number> = new Subject<number>();
+  public navWidth: Subject<number> = new Subject<number>();
 
-    public ngOnInit() {
-        this.containerWidth
-            .pipe(
-                debounceTime(100),
-                takeUntil(this.componentDestroyed$),
-            )
-            .subscribe((containerWidth) => {
-                const navWidth = this.nav.nativeElement.offsetWidth;
+  public ngOnInit() {
+    this.containerWidth
+      .pipe(
+        debounceTime(100),
+        takeUntil(this.componentDestroyed$),
+      )
+      .subscribe((containerWidth) => {
+        const navWidth = this.nav.nativeElement.offsetWidth;
 
-                if (navWidth > containerWidth) {
-                    this.removeItem();
-                } else if (this.extra.length > 0) {
-                   this.addItem();
-                }
-            });
-    }
-
-    public ngOnChanges() {
-        if (this.profile && this.profile.isAdmin) {
-            this.navItems = [
-                { title: ngxExtract('GENERAL.MENU.USERS'), link: ['/admin/users/overview'] },
-                { title: ngxExtract('GENERAL.MENU.COMPANIES'), link: ['/admin/companies'] },
-                { title: ngxExtract('GENERAL.MENU.SIGNUP-REQUESTS'), link: ['/admin/users/signup-requests'] },
-            ];
-        } else {
-            this.navItems = [
-                { title: ngxExtract('GENERAL.MENU.REPORTS'), link: ['/recycler/reports'] },
-                { title: ngxExtract('GENERAL.MENU.RECYCLINGPROCESSES'), link: ['/recycler/recycling-processes'] },
-                { title: ngxExtract('GENERAL.MENU.RECYCLINGPARTNERS'), link: ['/recycler/recycling-partners'] },
-                { title: ngxExtract('GENERAL.MENU.PROXIES'), link: ['/recycler/proxies'] },
-                { title: ngxExtract('GENERAL.MENU.AUDITTRIAL'), link: ['/recycler/audit-trail'] },
-            ];
+        if (navWidth > containerWidth) {
+          this.removeItem();
+        } else if (this.extra.length > 0) {
+          this.addItem();
         }
+      });
+  }
 
-        this.filteredNavItems = [...this.navItems];
-
-        setTimeout(() => {
-            this.setWidth();
-        }, 50);
+  public ngOnChanges() {
+    if (this.profile && this.profile.isAdmin) {
+      this.navItems = [
+        { title: ngxExtract('GENERAL.MENU.USERS'), link: ['/admin/users/overview'] },
+        { title: ngxExtract('GENERAL.MENU.COMPANIES'), link: ['/admin/companies'] },
+        { title: ngxExtract('GENERAL.MENU.SIGNUP-REQUESTS'), link: ['/admin/users/signup-requests'] },
+      ];
+      // TODO: get types from enum
+    } else if (this.profile && this.profile.company.meta.type === 'CO') {
+      this.navItems = [
+        { title: ngxExtract('GENERAL.MENU.REPORTS'), link: ['/recycler/reports'] },
+        { title: ngxExtract('GENERAL.MENU.PROXIES'), link: ['/recycler/proxies'] },
+      ];
+        // TODO: get types from enum
+    } else if (this.profile && this.profile.company.meta.type === 'AO') {
+      this.navItems = [
+        { title: ngxExtract('GENERAL.MENU.REPORTS'), link: ['/recycler/reports'] },
+      ];
+    } else {
+      this.navItems = [
+        { title: ngxExtract('GENERAL.MENU.REPORTS'), link: ['/recycler/reports'] },
+        { title: ngxExtract('GENERAL.MENU.RECYCLINGPROCESSES'), link: ['/recycler/recycling-processes'] },
+        { title: ngxExtract('GENERAL.MENU.RECYCLINGPARTNERS'), link: ['/recycler/recycling-partners'] },
+        { title: ngxExtract('GENERAL.MENU.PROXIES'), link: ['/recycler/proxies'] },
+        { title: ngxExtract('GENERAL.MENU.AUDITTRIAL'), link: ['/recycler/audit-trail'] },
+      ];
     }
 
-    public ngAfterViewInit() {
+    this.filteredNavItems = [...this.navItems];
 
+    setTimeout(() => {
+      this.setWidth();
+    }, 50);
+  }
+
+  public ngAfterViewInit() {
+
+  }
+
+  public ngOnDestroy() {
+    this.componentDestroyed$.next(true);
+    this.componentDestroyed$.complete();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  public onResize(event) {
+    this.setWidth();
+  }
+
+  public setWidth() {
+    if (this.container && this.nav) {
+      this.containerWidth.next(this.container.nativeElement.offsetWidth);
+      this.navWidth.next(this.nav.nativeElement.offsetWidth);
     }
+  }
 
-    public ngOnDestroy() {
-        this.componentDestroyed$.next(true);
-        this.componentDestroyed$.complete();
-    }
+  private removeItem() {
+    this.filteredNavItems = dropLast(1, this.filteredNavItems);
+    this.extra = takeLast(this.navItems.length - this.filteredNavItems.length, this.navItems);
 
-    @HostListener('window:resize', ['$event'])
-    public onResize(event) {
-        this.setWidth();
-    }
+    setTimeout(() => {
+      const containerWidth = this.container.nativeElement.offsetWidth;
+      const navWidth = this.nav.nativeElement.offsetWidth;
 
-    public setWidth() {
-        if (this.container && this.nav) {
-            this.containerWidth.next(this.container.nativeElement.offsetWidth);
-            this.navWidth.next(this.nav.nativeElement.offsetWidth);
-        }
-    }
+      if (navWidth > containerWidth) {
+        this.removeItem();
+      }
+    }, 1);
+  }
 
-    private removeItem() {
-        this.filteredNavItems = dropLast(1, this.filteredNavItems);
-        this.extra = takeLast(this.navItems.length - this.filteredNavItems.length, this.navItems);
+  private addItem() {
+    this.filteredNavItems = [...this.filteredNavItems, ...take(1, this.extra)];
+    this.extra = drop(1, this.extra);
 
-        setTimeout(() => {
-            const containerWidth = this.container.nativeElement.offsetWidth;
-            const navWidth = this.nav.nativeElement.offsetWidth;
+    setTimeout(() => {
+      const containerWidth = this.container.nativeElement.offsetWidth;
+      const navWidth = this.nav.nativeElement.offsetWidth;
 
-            if (navWidth > containerWidth) {
-                this.removeItem();
-            }
-        }, 1);
-    }
-
-    private addItem() {
-        this.filteredNavItems = [...this.filteredNavItems, ...take(1, this.extra)];
-        this.extra = drop(1, this.extra);
-
-        setTimeout(() => {
-            const containerWidth = this.container.nativeElement.offsetWidth;
-            const navWidth = this.nav.nativeElement.offsetWidth;
-
-            if (this.extra.length > 0 && navWidth < containerWidth) {
-                this.addItem();
-            } else if (navWidth > containerWidth) {
-                this.removeItem();
-            }
-        }, 1);
-    }
+      if (this.extra.length > 0 && navWidth < containerWidth) {
+        this.addItem();
+      } else if (navWidth > containerWidth) {
+        this.removeItem();
+      }
+    }, 1);
+  }
 }
