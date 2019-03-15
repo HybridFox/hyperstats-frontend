07a4765b5b6d48d3e78878a4bd5b2404686a43d6
Jo Smets <josmets1997@gmail.com>
2019-03-15 09:45:13 +0100

    chore: add validation errors for additives

--- client/src/app/reports/pages/detail/pages/additives/additives-validator.directive.ts
+++ client/src/app/reports/pages/detail/pages/additives/additives-validator.directive.ts
@@ -5,10 +5,19 @@ import { debug } from 'util';
 export function validateAdditives(control: FormGroup) {
   const type = control.get('type').value;
   const weight = control.get('weight').value;
+
   if (!isEmpty(type) && weight) {
+    control.controls['type'].setErrors(null);
+    control.controls['weight'].setErrors(null);
     return null;
   } else if (!isEmpty(type) || weight) {
+    Object.keys(control.controls).map((controlName) => {
+      control.get(controlName).markAsDirty({onlySelf: true});
+    });
+    weight ? control.controls['type'].setErrors({'typeRequired': true}) : control.controls['weight'].setErrors({'weightRequired': true});
     return { isRequired: true };
   }
+  control.controls['type'].setErrors(null);
+  control.controls['weight'].setErrors(null);
   return null;
 }
--- client/src/app/reports/pages/detail/pages/step-wrapper/step-wrapper.page.ts
+++ client/src/app/reports/pages/detail/pages/step-wrapper/step-wrapper.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, DoCheck } from '@angular/core';
+import { Component } from '@angular/core';
 import { BehaviorSubject, Subject, combineLatest } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { ActivatedRoute, Router } from '@angular/router';
+import { takeUntil, filter } from 'rxjs/operators';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { select$, select } from '@angular-redux/store';
 import { map } from 'rxjs/operators';
 
@@ -14,7 +14,7 @@ import { FormArray, FormGroup } from '@angular/forms';
 @Component({
   templateUrl: './step-wrapper.page.html',
 })
-export class StepWrapperPageComponent implements OnInit, DoCheck {
+export class StepWrapperPageComponent {
   @select$(ReportsProcessSelector.detail.result, mapToStepMenuItems) public siteMenuItems$: BehaviorSubject<StepMenuItem[]>;
   @select(ReportsProcessSelector.detail.result) public process$: BehaviorSubject<any>;
   public title$;
@@ -33,14 +33,15 @@ export class StepWrapperPageComponent implements OnInit, DoCheck {
     private route: ActivatedRoute,
     private router: Router,
     public formData: FormDataService,
-  ) {}
-
-  public ngOnInit() {
-    this.setForm();
-  }
-
-  public ngDoCheck() {
-    this.setForm();
+  ) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      const step = event.url.substr(event.url.lastIndexOf('/') + 1);
+      if (step === 'input-fraction' || step === 'output-fraction' || step === 'additives') {
+        this.setForm();
+      }
+    });
   }
 
   public setForm() {
