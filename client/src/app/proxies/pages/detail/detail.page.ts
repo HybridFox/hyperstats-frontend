e6e9067383dd7d4a20cf1d83134ae47e8e011f8c
Jochem Janssens <janssens.jochem@gmail.com>
2019-03-20 10:19:26 +0100

    feat: show selected years

--- client/src/app/proxies/pages/detail/detail.page.ts
+++ client/src/app/proxies/pages/detail/detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { select } from '@angular-redux/store';
 
+import { CodesService } from 'src/app/core/services/codes/codes.service';
 import { ProxiesActions, ProxiesSelectors } from '../../store';
 import { Proxy } from '../../store/types';
 
@@ -12,17 +13,33 @@ import { Proxy } from '../../store/types';
 export class DetailPageComponent implements OnInit {
   @select(ProxiesSelectors.list.result) public $proxies: Observable<Proxy[]>;
 
-  public proxies: Proxy[];
+  public proxies: any[];
+  public years: any[];
 
   constructor(
     private proxiesActions: ProxiesActions,
-  ) { }
+    private codesService: CodesService,
+  ) {}
 
   ngOnInit() {
     this.proxiesActions.fetchAll().toPromise();
 
+    this.years = this.codesService.years().map(year => year.value);
     this.$proxies.subscribe((proxies) => {
-      this.proxies = proxies;
+      if (proxies) {
+        this.proxies = proxies.map(proxy => ({
+          proxyCompanyId: proxy.proxyCompanyId,
+          processes: proxy.processes.map(process => ({
+            process: process.process.data.name,
+            reports: this.years.map(year => (
+              {
+                year: parseInt(year, 10),
+                active: process.reports.filter(report => report.data.information.reportingYear === parseInt(year, 10)).length > 0,
+              }
+            )),
+          }))
+        }));
+      }
     });
   }
 
--- client/src/app/proxies/store/types.ts
+++ client/src/app/proxies/store/types.ts
@@ -1,3 +1,27 @@
+import { Information } from 'src/app/reports/store/reports/types';
+
 export interface Proxy {
-  type: any;
+  proxyCompanyId: string;
+  processes: ProxyProcess[];
+}
+
+interface ProxyProcess {
+  process: {
+    _id: string;
+    data: {
+      name: string;
+    };
+  };
+  reports: ProxyReport[];
+}
+
+interface ProxyReport {
+  _id: string;
+  meta: {
+    created: string;
+    status: string;
+  };
+  data: {
+    information: Information;
+  };
 }
