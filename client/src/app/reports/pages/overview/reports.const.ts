d9bfb245ae590e5faea298c7cfb31438f8754f12
Jo Smets <josmets1997@gmail.com>
2019-03-22 07:51:40 +0100

    chore: sorting on reports for CO and AO

--- client/src/app/reports/pages/overview/reports.const.ts
+++ client/src/app/reports/pages/overview/reports.const.ts
@@ -19,3 +19,17 @@ export const SORTOPTIONS = [{
     label: ngxExtract('CONSTANTS.SORTOPTIONS.STATUS-DRAFT-FILED'),
     value: '-status'
 }];
+
+export const SORTOPTIONS_SHARED_REPORTS = [{
+  label: ngxExtract('CONSTANTS.SORTOPTIONS.NAME'),
+  value: 'name'
+}, {
+  label: ngxExtract('CONSTANTS.SORTOPTIONS.LAST-EDITED'),
+  value: '-lastUpdated'
+}, {
+  label: ngxExtract('CONSTANTS.SORTOPTIONS.REPORTING-YEAR-OTN'),
+  value: 'reportingYear'
+}, {
+  label: ngxExtract('CONSTANTS.SORTOPTIONS.REPORTING-YEAR-NTO'),
+  value: '-reportingYear'
+}];
