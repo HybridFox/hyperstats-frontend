e23be399a3af1d9d4d95886604c35662ea6ed4bd
Jochem Janssens <janssens.jochem@gmail.com>
2019-03-28 10:00:37 +0100

    feat: remove reports when a RP is removed

--- server/controllers/recyclingProcess/helpers/recyclingProcessManager/remove.js
+++ server/controllers/recyclingProcess/helpers/recyclingProcessManager/remove.js
@@ -1,13 +1,24 @@
 const RecyclingProcessModel = require("../../../../models/recyclingProcess");
+const ReportModel = require("../../../../models/report");
+const { REPORT_STATUS } = require("../../../report/helpers/const");
+
 const errors = require("../../../../helpers/errorHandler");
 
-module.exports = (id) => {
-	return RecyclingProcessModel.findOneAndUpdate({ _id: id, "meta.deleted": false }, { $set: { "meta.deleted": true } }).exec()
-		.then((response) => {
-			if (!response) {
-				throw errors.ProcessNotFound;
-			}
+module.exports = (id) => ReportModel.findOne({ "data.information.recyclingProcess": id, "meta.status": REPORT_STATUS.FILED }).exec()
+	.then((response) => {
+		if (response) {
+			throw errors.Forbidden;
+		}
 
-			return response;
-		});
-};
+		return;
+	})
+	.then(() => RecyclingProcessModel.updateOne({ _id: id, "meta.deleted": false }, { $set: { "meta.deleted": true } }).exec())
+	.then((response) => {
+		if (!response.nModified) {
+			throw errors.ProcessNotFound;
+		}
+
+		return;
+	})
+	.then(() => ReportModel.updateMany({ "data.information.recyclingProcess": id }, { $set: { "meta.deleted": true } }).exec());
+
