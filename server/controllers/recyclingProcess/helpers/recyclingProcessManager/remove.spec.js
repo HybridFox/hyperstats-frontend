89aef99b9fb901d3b9282fb7390cf2d809402bb3
Jochem Janssens <janssens.jochem@gmail.com>
2019-03-28 14:13:36 +0100

    feat: added test for remove RP

--- server/controllers/recyclingProcess/helpers/recyclingProcessManager/remove.spec.js
+++ server/controllers/recyclingProcess/helpers/recyclingProcessManager/remove.spec.js
@@ -1,18 +1,26 @@
 
 const { expect, use, should } = require("chai");
+const { set, lensPath } = require("ramda");
 const chaiAsPromised = require("chai-as-promised");
 const createObjectId = require("mongoose").Types.ObjectId;
 const Model = require("../../../../models/recyclingProcess");
 const { mockMongoose } = require("../../../../test/mocks");
 const mockProcesses = require("../../../../test/mocks/recyclingProcesses");
 
+const { report: reportMock } = require("../../../../test/mocks");
+const createTestUser = require("../../../../test/helpers/createTestUser");
+const createTestReport = require("../../../../test/helpers/testReport");
+const ReportModel = require("../../../../models/report");
+
 should();
 use(chaiAsPromised);
 
-describe("Remove Recycling process", () => {
+describe.only("Remove Recycling process", () => {
 	let remove;
 	let mongoServer;
 	let processId;
+	let reportId;
+	let companyOfUser;
 	const companyId = createObjectId();
 	const RecyclingProcesses = mockProcesses();
 
@@ -22,8 +30,24 @@ describe("Remove Recycling process", () => {
 		const create = require("./create");
 
 		const result = await create({ process: RecyclingProcesses[0].data, companyId });
-
 		processId = result._id;
+
+		const user = await createTestUser();
+		companyOfUser = user.data.company;
+
+		let mockReport = set(
+			lensPath(["data", "information", "recyclingProcess"]),
+			processId,
+			reportMock.mock
+		);
+
+		mockReport = set(
+			lensPath(["meta", "status"]),
+			"SAVED",
+			mockReport
+		);
+
+		reportId = (await createTestReport.create(companyOfUser, mockReport))._id;
 	});
 
 	after(() => {
@@ -34,6 +58,13 @@ describe("Remove Recycling process", () => {
 		await remove(processId);
 		const result = await Model.findOne({ _id: processId }).exec();
 
+		expect(result).to.be.an("object");
+		expect(result.meta).to.be.an("object");
+		expect(result.meta.deleted).to.be.true;
+	});
+
+	it("Should remove the corresponding saved reports", async() => {
+		const result = await ReportModel.findById(reportId).lean().exec();
 		expect(result).to.be.an("object");
 		expect(result.meta).to.be.an("object");
 		expect(result.meta.deleted).to.be.true;
