2c5753c0325a423153e35db4d0f751d2f9e5d74b
Jochem Janssens <janssens.jochem@gmail.com>
2019-04-04 16:00:32 +0200

    feat: improve itemName of company

--- server/controllers/auditLogs/helpers/create.js
+++ server/controllers/auditLogs/helpers/create.js
@@ -1,7 +1,22 @@
 const AuditLogModel = require("../../../models/auditLog");
+const recyclingProcessManager = require("../../recyclingProcess/helpers/recyclingProcessManager");
+const { getOne } = require("../../company/helpers");
+
+const { pathOr } = require("ramda");
 
 module.exports = async({ item, type, user, action = "created" }) => {
-	const itemName = (type === "proxy") ? `${item.recyclingProcess} in ${item.year}` : item.data.information.name;
+	const recyclingProcess = await recyclingProcessManager.getById({
+		_id: item.recyclingProcess,
+		createdByCompany: pathOr(null, ["data", "company", "_id"], user),
+	});
+
+	const company = await getOne({
+		_id: item.proxy,
+		companyOfUser: item.proxy,
+		isAdmin: false,
+	});
+
+	const itemName = (type === "proxy") ? `${recyclingProcess.data.name} in ${item.year} to ${company.data.name}` : item.data.information.name;
 
 	const newLog = new AuditLogModel({
 		data: {
