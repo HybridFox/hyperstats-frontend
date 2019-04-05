2b2d68569b77c9f93e5215191ce2352d804b39ae
Jochem Janssens <janssens.jochem@gmail.com>
2019-04-05 09:22:28 +0200

    feat: add audit log item on remove report

--- server/controllers/recyclingProcess/remove.js
+++ server/controllers/recyclingProcess/remove.js
@@ -1,9 +1,13 @@
 const recyclingProcessManager = require("./helpers/recyclingProcessManager");
+const profileHelper = require("../../helpers/profile");
 
-module.exports = (req, res, next) => {
+module.exports = async(req, res, next) => {
+	const profile = await profileHelper.getFull(req);
 	const id = req.data.params.id;
-	recyclingProcessManager.remove(id)
+
+	recyclingProcessManager.remove(id, profile)
 		.then(() => {
 			res.status(204).send();
-		}, next);
+		})
+		.catch(next);
 };
